c0cc7b31fcf1a1de65b83101ea9ba6b24eea7943
emiliano.morales <emiliano.morales@bizitglobal.com>
2024-04-14 17:45:14 -0300

    crud teachers works

--- src/app/layouts/dashboard/pages/teachers/components/teachers-dialog/teachers-dialog.component.ts
+++ src/app/layouts/dashboard/pages/teachers/components/teachers-dialog/teachers-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ITeachers } from '../../models/teachers_iface';
 
 @Component({
   selector: 'app-teachers-dialog',
@@ -6,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./teachers-dialog.component.scss']
 })
 export class TeachersDialogComponent {
+  teachersForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private matDialogRef: MatDialogRef<TeachersDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) private editingTeacher?: ITeachers){
+                
+              this.teachersForm = this.formBuilder.group({
+                id: [editingTeacher?.id || ''], 
+                name: ['', [Validators.required]],
+                lastName: ['', [Validators.required]],
+                age: ['', [Validators.required]],
+              }); 
+              
+            if (editingTeacher){
+            this.teachersForm.patchValue(editingTeacher);
+    }
+  }
+
+  get nameControl() {
+    return this.teachersForm.get('asignature');
+  }
+
+  get lastNameControl() {
+    return this.teachersForm.get('professor');
+  }
+
+  get ageControl() {
+    return this.teachersForm.get('asignatureType');
+  }
+
+  onSave(): void{
+    if(this.teachersForm.invalid){ // Invalid form
+      this.teachersForm.markAllAsTouched();
+    }else{ // Valid form
+      this.matDialogRef.close(this.teachersForm.value);
+    }
+  }
 
 }
