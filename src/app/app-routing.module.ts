02f847e4c8ab968941391b8995b044cb0a798b95
emiliano.morales <emiliano.morales@bizitglobal.com>
2024-05-12 22:09:27 -0300

    routing to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 import { AuthComponent } from './layouts/auth/auth.component';
+import { DashboardComponent } from './layouts/dashboard/dashboard.component';
+import { AuthGuard } from './core/guards/auth.guards';
 
 const routes: Routes = [
-  { path:'', pathMatch:'full', redirectTo: 'dashboard/students' },
-  { path:'auth', component: AuthComponent },
-  { path:'dashboard', component: DashboardComponent, loadChildren: () => import('./layouts/dashboard/dashboard.module').then((m) => m.DashboardModule) },
-  { path:'**', redirectTo: '/dashboard/students' },  
+  { path: '', redirectTo: 'auth', pathMatch: 'full' }, 
+  { path: 'auth', component: AuthComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard], 
+    loadChildren: () =>
+      import('./layouts/dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  { path: '**', redirectTo: 'auth' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
